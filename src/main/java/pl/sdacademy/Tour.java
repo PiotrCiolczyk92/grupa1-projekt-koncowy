2a035bab90f7032447bb6143558de406abd38be2
Justyna <justyna.budzioch@gmail.com>
2021-10-16 15:51:42 +0200

    all tours - method

--- src/main/java/pl/sdacademy/Tour.java
+++ src/main/java/pl/sdacademy/Tour.java
@@ -1,11 +1,14 @@
 package pl.sdacademy;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+
 public class Tour {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +17,19 @@ public class Tour {
     private String country;
     private Integer price;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public Integer getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
 }
--- src/main/java/pl/sdacademy/TourController.java
+++ src/main/java/pl/sdacademy/TourController.java
@@ -1,9 +1,26 @@
 package pl.sdacademy;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/tour")
 public class TourController {
+    private TourRepository tourRepository;
+
+    public TourController(TourRepository tourRepository) {
+        this.tourRepository = tourRepository;
+    }
+
+    @GetMapping("/list")
+    public String getList(ModelMap modelMap) {
+        List<Tour> tours = tourRepository.findAll();
+        modelMap.addAttribute("tours", tours);
+        return "list";
+    }
 }

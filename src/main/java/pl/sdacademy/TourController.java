dedefebaf29b208563f8eb3875b7b8aa70dc8899
Bartek <koszowski.b@gmail.com>
2021-10-16 14:08:16 +0100

    dodanie klasy TourController z adnotacjami @Controller oraz @RequestMapping

---
+++ src/main/java/pl/sdacademy/TourController.java
@@ -0,0 +1,9 @@
+package pl.sdacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tour")
+public class TourController {
+}

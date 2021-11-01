87b0fa63109e32f7132c2ae58ab5a9cd94d96914
Piotr <piotrciolczyk992@gmail.com>
2021-11-01 22:45:32 +0100

    Wyrzucenie enum Country. Poprawa widokow airport i tworzenia przelotu.

--- src/main/java/pl/sdacademy/entities/Location.java
+++ src/main/java/pl/sdacademy/entities/Location.java
@@ -2,8 +2,6 @@ package pl.sdacademy.entities;
 
 import lombok.Getter;
 import lombok.Setter;
-import pl.sdacademy.enums.Country;
-
 import javax.persistence.*;
 
 @Entity
@@ -13,12 +11,11 @@ public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Enumerated(EnumType.STRING)
-    private Country country;
+    private String country;
     private String region;
     private String city;
 
     public String getFullLocation() {
-        return "Kraj: " + country + " Region: " + region + " Miasto: " + city;
+        return "Kraj: " + country + " / Region: " + region + " / Miasto: " + city;
     }
 }
